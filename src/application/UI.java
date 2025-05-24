cfebf852a87058500d8e145d9bc08a822b02e3f6
SaidAsaad <saidyousef201513@gmail.com>
2025-05-23 23:42:38 -0300

    Handling exceptions and clearing scream

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,7 +8,8 @@ import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-
+	
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 	public static final String ANSI_RESET1 = "\u001B[0m";
 	public static final String ANSI_BLACK1 = "\u001B[30m";
 	public static final String ANSI_RED1 = "\u001B[31m";
@@ -46,15 +47,21 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
+
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 	
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
-		}
-		catch (RuntimeException e){
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
 		}
 	}
@@ -72,17 +79,15 @@ public class UI {
 
 	public static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-            System.out.print("-");
-        }
-        else {
-            if (piece.getColor() == Color.WHITE) {
-                System.out.print(ANSI_WHITE1 + piece + ANSI_RESET1);
-            }
-            else {
-                System.out.print(ANSI_YELLOW1 + piece + ANSI_RESET1);
-            }
-        }
-        System.out.print(" ");
+			System.out.print("-");
+		} else {
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(ANSI_WHITE1 + piece + ANSI_RESET1);
+			} else {
+				System.out.print(ANSI_YELLOW1 + piece + ANSI_RESET1);
+			}
+		}
+		System.out.print(" ");
 	}
 
 }
